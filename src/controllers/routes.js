ab23ebcda1f4d61202e8a9974ccde4dc2096b5c3
Andy Choo <andychoo7005@gmail.com>
2022-03-23 15:34:11 +0800

    latest

--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -2,21 +2,21 @@ const express = require('express');
 
 const authController = require('#controllers/auth');
 const profileController = require('#controllers/profile');
+const isLoggedIn = require('#middlewares/isLoggedIn');
 
 const router = express.Router();
 
 // auth controller
 router.post('/auth/register', authController.register);
 router.post('/auth/forgot-password', authController.forgotPassword);
-router.post('/auth/reset-password', authController.resetPassword);
-router.post('/auth/register-profile', authController.registerProfile);
+router.post('/auth/reset-password' , authController.resetPassword);
+router.post('/auth/register-profile', isLoggedIn ,authController.registerProfile);
 
 // profile controller
-router.get('/profile', profileController.getProfile);
-router.put('/profile', profileController.updateProfile);
+router.get('/profile', isLoggedIn,profileController.getProfile);
+router.put('/profile', isLoggedIn,profileController.updateProfile);
 
 // TO DO LIST
-// 1. forget password.
-// 2. update profile.
+// check whether user does exist in both firebase and mongodb. if no, throw error and redirect them to register profile.
 
 module.exports = router;
